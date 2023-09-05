be81a5cf1ba33e87d46d386da46b67d6bf724b69
guhandelta <guhan.delta@gmail.com>
2023-09-05 10:44:18 -0400

    Connected the UI to the GEO Ipify API to fetch the IP address information

--- src/components/IPAddress.jsx
+++ src/components/IPAddress.jsx
@@ -3,12 +3,39 @@ import SearchBar from "./SearchBar"
 import Stats from "./Stats"
 
 const IPAddress = () => {
-   
+    const [ ipAddress, setIPAddrss ] = useState('');
+    const [ location, setLocation ] = useState('');
+    const [ timezone, setTimezone ] = useState('');
+    const [ ISP, setISP ] = useState('');
+    const [ coordinates, setCoordinates ] = useState({});
+
+    const fetchLocation = (ipAddress="") =>{
+        fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_GEOIPIFY_API_KEY}&ipAddress=${ipAddress}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            setIPAddrss(data.ip);
+            setLocation(`${data.location.city}, ${data.location.country}, ${data.location.postalCode}`)
+            setTimezone(`UTC ${data.location.timezone}`)
+            setISP(`${data.isp}`);
+            setCoordinates({
+                lat: data.location.lat,
+                lng: data.location.lng,
+            });
+        });
+    }
+
+    useEffect(() => fetchLocation(), []);
 
   return (
     <div className="flex flex-col h-screen relative">
         <SearchBar />
-        <Stats />
+        <Stats 
+            ipAddress={ipAddress}
+            location={location}
+            timezone={timezone}
+            isp={ISP}
+        />
     </div>
   )
 }
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 
-const Stats = () => {
+const Stats = ({ ipAddress, location, timezone, isp }) => {
   return (
     <div className="w-4/5 rounded-lg absolute top-52 md:top-56 left-10 md:left-48 bg-whitez-10 flex flex-col md:flex-row container gap-4 md:gap-10 p-4 md:p-8">
         <div className="md:border-r-2 w-full border-slate-300 flex flex-col gap items-center gap-2">
-            <p className="text-sm text-slate-400 fony-semibold"></p>
-            <div className="md:text-2xl text-black font-bold">IP Address</div>
+            <p className="text-sm text-slate-400 fony-semibold">IP Address</p>
+            <div className="md:text-xl text-black font-bold">{ipAddress}</div>
         </div>
         <div className="md:border-r-2 w-full border-slate-300 flex flex-col gap items-center gap-2">
             <p className="text-sm text-slate-400 fony-semibold">Location</p>
-            <div className="md:text-2xl text-black font-bold"></div>
+            <div className="md:text-xl text-black font-bold">{location}</div>
         </div>
         <div className="md:border-r-2 w-full border-slate-300 flex flex-col gap items-center gap-2">
             <p className="text-sm text-slate-400 fony-semibold">Time Zone</p>
-            <div className="md:text-2xl text-black font-bold"></div>
+            <div className="md:text-xl text-black font-bold">{timezone}</div>
         </div>
         <div className="w-full flex flex-col gap-2 items-center">
             <p className="text-sm text-slate-400 fony-semibold">ISP</p>
-            <div className="md:text-2xl text-black font-bold"></div>
+            <div className="md:text-xl text-black font-bold">{isp}</div>
         </div>
     </div>
   )
